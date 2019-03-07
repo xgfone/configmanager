cb29357ef7bf7c7d790b1b3d01b263b3cce5c925
xgfone <xgfone@126.com>
2019-03-07 17:34:55 +0800

    use github.com/xgfone/go-tools/types

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xgfone/go-config
+
+require github.com/xgfone/go-tools v5.2.0+incompatible
--- utils.go
+++ utils.go
@@ -18,234 +18,22 @@ package config
 
 import (
 	"fmt"
-	"html/template"
-	"strconv"
 	"strings"
 	"time"
+
+	"github.com/xgfone/go-tools/types"
 )
 
-func inMap(m map[string]interface{}, key string) bool {
-	_, ok := m[key]
-	return ok
-}
+// Some converting function aliases.
+var (
+	IsZero = types.IsZero
 
-// IsZero returns true if the value is ZERO, or false.
-func IsZero(v interface{}) bool {
-	ok, _ := template.IsTrue(v)
-	return !ok
-}
-
-// bool2Int64 converts the bool to int64.
-func bool2Int64(b bool) int64 {
-	if b {
-		return 1
-	}
-	return 0
-}
-
-// ToBool does the best to convert any certain value to bool.
-//
-// When the value is string, for "t", "T", "1", "on", "On", "ON", "true",
-// "True", "TRUE", it's true, for "f", "F", "0", "off", "Off", "OFF", "false",
-// "False", "FALSE", "", it's false.
-//
-// For other types, if the value is ZERO of the type, it's false. Or it's true.
-func ToBool(v interface{}) (bool, error) {
-	switch _v := v.(type) {
-	case nil:
-		return false, nil
-	case bool:
-		return _v, nil
-	case string:
-		switch _v {
-		case "t", "T", "1", "on", "On", "ON", "true", "True", "TRUE":
-			return true, nil
-		case "f", "F", "0", "off", "Off", "OFF", "false", "False", "FALSE", "":
-			return false, nil
-		default:
-			return false, fmt.Errorf("unrecognized bool string: %s", _v)
-		}
-	}
-	return !IsZero(v), nil
-}
-
-// ToInt64 does the best to convert any certain value to int64.
-func ToInt64(_v interface{}) (v int64, err error) {
-	switch t := _v.(type) {
-	case nil:
-	case bool:
-		v = bool2Int64(t)
-	case string:
-		v, err = strconv.ParseInt(t, 10, 64)
-	case int:
-		v = int64(t)
-	case int8:
-		v = int64(t)
-	case int16:
-		v = int64(t)
-	case int32:
-		v = int64(t)
-	case int64:
-		v = t
-	case uint:
-		v = int64(t)
-	case uint8:
-		v = int64(t)
-	case uint16:
-		v = int64(t)
-	case uint32:
-		v = int64(t)
-	case uint64:
-		v = int64(t)
-	case float32:
-		v = int64(t)
-	case float64:
-		v = int64(t)
-	case complex64:
-		v = int64(real(t))
-	case complex128:
-		v = int64(real(t))
-	default:
-		err = fmt.Errorf("unknown type of %T", _v)
-	}
-	return
-}
-
-// ToUint64 does the best to convert any certain value to uint64.
-func ToUint64(_v interface{}) (v uint64, err error) {
-	switch t := _v.(type) {
-	case nil:
-	case bool:
-		v = uint64(bool2Int64(t))
-	case string:
-		v, err = strconv.ParseUint(t, 10, 64)
-	case int:
-		v = uint64(t)
-	case int8:
-		v = uint64(t)
-	case int16:
-		v = uint64(t)
-	case int32:
-		v = uint64(t)
-	case int64:
-		v = uint64(t)
-	case uint:
-		v = uint64(t)
-	case uint8:
-		v = uint64(t)
-	case uint16:
-		v = uint64(t)
-	case uint32:
-		v = uint64(t)
-	case uint64:
-		v = t
-	case float32:
-		v = uint64(t)
-	case float64:
-		v = uint64(t)
-	case complex64:
-		v = uint64(real(t))
-	case complex128:
-		v = uint64(real(t))
-	default:
-		err = fmt.Errorf("unknown type of %T", _v)
-	}
-	return
-}
-
-// ToFloat64 does the best to convert any certain value to float64.
-func ToFloat64(_v interface{}) (v float64, err error) {
-	switch t := _v.(type) {
-	case nil:
-	case bool:
-		v = float64(bool2Int64(t))
-	case string:
-		v, err = strconv.ParseFloat(t, 64)
-	case int:
-		v = float64(t)
-	case int8:
-		v = float64(t)
-	case int16:
-		v = float64(t)
-	case int32:
-		v = float64(t)
-	case int64:
-		v = float64(t)
-	case uint:
-		v = float64(t)
-	case uint8:
-		v = float64(t)
-	case uint16:
-		v = float64(t)
-	case uint32:
-		v = float64(t)
-	case uint64:
-		v = float64(t)
-	case float32:
-		v = float64(t)
-	case float64:
-		v = t
-	case complex64:
-		v = float64(real(t))
-	case complex128:
-		v = real(t)
-	default:
-		err = fmt.Errorf("unknown type of %T", _v)
-	}
-	return
-}
-
-// ToString does the best to convert any certain value to string.
-//
-// For time.Time, it will use time.RFC3339Nano to format it.
-func ToString(_v interface{}) (v string, err error) {
-	switch t := _v.(type) {
-	case nil:
-	case string:
-		v = t
-	case []byte:
-		v = string(t)
-	case bool:
-		if t {
-			v = "true"
-		} else {
-			v = "false"
-		}
-	case int:
-		v = strconv.FormatInt(int64(t), 10)
-	case int8:
-		v = strconv.FormatInt(int64(t), 10)
-	case int16:
-		v = strconv.FormatInt(int64(t), 10)
-	case int32:
-		v = strconv.FormatInt(int64(t), 10)
-	case int64:
-		v = strconv.FormatInt(t, 10)
-	case uint:
-		v = strconv.FormatUint(uint64(t), 10)
-	case uint8:
-		v = strconv.FormatUint(uint64(t), 10)
-	case uint16:
-		v = strconv.FormatUint(uint64(t), 10)
-	case uint32:
-		v = strconv.FormatUint(uint64(t), 10)
-	case uint64:
-		v = strconv.FormatUint(t, 10)
-	case float32:
-		v = strconv.FormatFloat(float64(t), 'f', -1, 32)
-	case float64:
-		v = strconv.FormatFloat(t, 'f', -1, 64)
-	case error:
-		v = t.Error()
-	case time.Time:
-		v = t.Format(time.RFC3339Nano)
-	case fmt.Stringer:
-		v = t.String()
-	default:
-		err = fmt.Errorf("unknown type of %T", _v)
-	}
-	return
-}
+	ToBool    = types.ToBool
+	ToInt64   = types.ToInt64
+	ToUint64  = types.ToUint64
+	ToFloat64 = types.ToFloat64
+	ToString  = types.ToString
+)
 
 // ToStringSlice does the best to convert a certain value to []string.
 //
@@ -282,7 +70,7 @@ func ToIntSlice(_v interface{}) (v []int, err error) {
 				continue
 			}
 
-			i, err := ToInt64(s)
+			i, err := types.ToInt64(s)
 			if err != nil {
 				return nil, err
 			}
@@ -309,7 +97,7 @@ func ToInt64Slice(_v interface{}) (v []int64, err error) {
 				continue
 			}
 
-			i, err := ToInt64(s)
+			i, err := types.ToInt64(s)
 			if err != nil {
 				return nil, err
 			}
@@ -336,7 +124,7 @@ func ToUintSlice(_v interface{}) (v []uint, err error) {
 				continue
 			}
 
-			i, err := ToUint64(s)
+			i, err := types.ToUint64(s)
 			if err != nil {
 				return nil, err
 			}
@@ -363,7 +151,7 @@ func ToUint64Slice(_v interface{}) (v []uint64, err error) {
 				continue
 			}
 
-			i, err := ToUint64(s)
+			i, err := types.ToUint64(s)
 			if err != nil {
 				return nil, err
 			}
@@ -390,7 +178,7 @@ func ToFloat64Slice(_v interface{}) (v []float64, err error) {
 				continue
 			}
 
-			i, err := ToFloat64(s)
+			i, err := types.ToFloat64(s)
 			if err != nil {
 				return nil, err
 			}
